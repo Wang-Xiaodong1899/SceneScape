7289f23374bbb4d41639df6f811250debf766d0c
Xiaodong Wang (MAC) <1443472206@qq.com>
2023-09-24 00:21:43 +0800

    fix

--- models/dp.py
+++ models/dp.py
@@ -284,7 +284,7 @@ class WarpInpaintModel(torch.nn.Module):
         # print(f'norm depth min: {updated_depth.min()}, max: {updated_depth.max()}')
 
         # scale = 5
-        updated_depth = updated_depth * 20
+        updated_depth = updated_depth * 40
         print(f'update depth min: {updated_depth.min()}, max: {updated_depth.max()}')
 
         updated_depth[mask] = -1
